fc1537db862c17ac5de1a5d3e9fcc1a56afce1c7
Mathieu Rhéaume <mrheaume@cortexmedia.ca>
2015-05-14 10:14:17 -0400

    Updated beluga receipe

--- beluga.rb
+++ beluga.rb
@@ -2,7 +2,9 @@ require 'formula'
 
 class Beluga < Formula
   homepage 'https://github.com/cortexmedia/Beluga'
-  head 'https://github.com/cortexmedia/Beluga.git'
+  # Use the tag instead of the tarball to get submodules
+  url "https://github.com/cortexmedia/Beluga.git", :tag => "0.0.1"
+  head "https://github.com/cortexmedia/Beluga.git"
 
   depends_on "docker" => :recommended
   depends_on "boot2docker" => :recommended
