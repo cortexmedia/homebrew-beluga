c465abbc01ae4e5af232e0ebfd846047fc0cfe0d
Mathieu Rhéaume <mrheaume@cortexmedia.ca>
2015-05-14 10:03:40 -0400

    Added beluga homebrew receipe

---
+++ beluga.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Beluga < Formula
+  homepage 'https://github.com/cortexmedia/Beluga'
+  head 'https://github.com/cortexmedia/Beluga.git'
+
+  depends_on "docker" => :recommended
+  depends_on "boot2docker" => :recommended
+
+  def install
+    bin.install "bin/beluga" => "beluga"
+    system 'make', 'install', "PREFIX=#{prefix}"
+  end
+end
