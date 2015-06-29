bf0a06ac28fbec8dedbc405476d85b1cde442aec
mturcottecortex <mturcotte@cortexmedia.ca>
2015-06-29 12:31:43 -0400

    Update version

--- beluga.rb
+++ beluga.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Beluga < Formula
   homepage 'https://github.com/cortexmedia/Beluga'
   # Use the tag instead of the tarball to get submodules
-  url "https://github.com/cortexmedia/Beluga.git", :tag => "0.3"
+  url "https://github.com/cortexmedia/Beluga.git", :tag => "0.4"
   head "https://github.com/cortexmedia/Beluga.git"
 
   depends_on "docker-compose" => :recommended
